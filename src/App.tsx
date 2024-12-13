07a306fc88e59e3d8a4c3268436009df8acd4bdb
Joel Ymele <jymele@yahoo.fr>
2024-12-13 16:37:53 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Experience from "./Experience";
+
+function App() {
+  return (
+    <>
+      <Experience />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Experience.tsx
@@ -0,0 +1,11 @@
+import { Canvas } from "@react-three/fiber";
+
+export default function Experience() {
+  return (
+    <>
+      <Canvas>
+        <mesh></mesh>
+      </Canvas>
+    </>
+  );
+}
