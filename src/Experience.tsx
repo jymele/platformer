313098aac65efd9bcb3501e6c55b53f05f8985ac
Joel Ymele <jymele@yahoo.fr>
2024-12-13 16:51:59 -0500

    Added components to test on

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,10 +1,24 @@
 import { Canvas } from "@react-three/fiber";
+import { Terrain } from "../models/terrain";
+import { OrbitControls } from "@react-three/drei";
+import { Player } from "../models/player";
 
 export default function Experience() {
   return (
     <>
-      <Canvas>
-        <mesh></mesh>
+      <Canvas camera={{ position: [0, 5, -6] }}>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight
+          position={[-2, 10, 0]}
+          intensity={1}
+          castShadow
+          receiveShadow
+        />
+
+        {/* <Capsule args={[]} /> */}
+        <Player />
+        <Terrain />
       </Canvas>
     </>
   );
