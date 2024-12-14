3ac3c5c46043aeea934a7c63f4f1138bdd9f23fe
Joel Ymele <jymele@yahoo.fr>
2024-12-13 21:41:12 -0500

    Positioned the camera behind the character

--- controllers/CharacterController.tsx
+++ controllers/CharacterController.tsx
@@ -1,14 +1,50 @@
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import { Player } from "../models/player";
-import React from "react";
+import React, { useRef } from "react";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+// import { cameraPosition } from "three/webgpu";
 // import { Capsule } from "@react-three/drei";
 
 export default function CharacterController() {
+  const container = React.useRef<THREE.Group>(null);
+  const cameraTarget = React.useRef<THREE.Group>(null);
+  const cameraPosition = React.useRef<THREE.Group>(null);
+  const character = React.useRef<THREE.Group>(null);
+
+  const cameraWorldPosition = useRef(new THREE.Vector3());
+  const cameraLookAtWorldPosition = useRef(new THREE.Vector3());
+  const cameraLookAt = useRef(new THREE.Vector3());
+
+  useFrame(({ camera }) => {
+    if (
+      !container.current ||
+      !cameraTarget.current ||
+      !cameraPosition.current ||
+      !character.current
+    )
+      return;
+    cameraPosition.current.getWorldPosition(cameraWorldPosition.current);
+    camera.position.lerp(cameraWorldPosition.current, 0.1);
+
+    if (cameraTarget.current) {
+      cameraTarget.current.getWorldPosition(cameraLookAtWorldPosition.current);
+      cameraLookAt.current.lerp(cameraLookAtWorldPosition.current, 0.1);
+      camera.lookAt(cameraLookAt.current);
+    }
+  });
+
   return (
     <>
       <RigidBody colliders={false}>
-        <CapsuleCollider args={[0.16, 0.3]} />
-        <Player position-y={-0.29} />
+        <group ref={container}>
+          <group ref={cameraTarget} position-z={1.5} />
+          <group ref={cameraPosition} position-y={2.5} position-z={-4} />
+          <group ref={character}>
+            <Player position-y={-0.29} />
+          </group>
+        </group>
+        <CapsuleCollider args={[0.16, 0.32]} />
       </RigidBody>
     </>
   );
--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,7 +1,5 @@
 import { Canvas } from "@react-three/fiber";
 import { Terrain } from "../models/terrain";
-import { OrbitControls } from "@react-three/drei";
-// import { Player } from "../models/player";
 import CharacterController from "../controllers/CharacterController";
 import { Physics } from "@react-three/rapier";
 
@@ -9,7 +7,6 @@ export default function Experience() {
   return (
     <>
       <Canvas camera={{ position: [0, 5, -6] }}>
-        <OrbitControls />
         <ambientLight intensity={0.5} />
         <directionalLight
           position={[-2, 10, 0]}
