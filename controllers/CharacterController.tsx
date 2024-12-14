550004081095cde09a20b9b981bd832c897afb55
Joel Ymele <jymele@yahoo.fr>
2024-12-13 22:05:24 -0500

    Added walking and running

--- controllers/CharacterController.tsx
+++ controllers/CharacterController.tsx
@@ -1,12 +1,21 @@
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
+import * as Rapier from "@dimforge/rapier3d-compat";
 import { Player } from "../models/player";
 import React, { useRef } from "react";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
+import { useKeyboardControls } from "@react-three/drei";
 // import { cameraPosition } from "three/webgpu";
 // import { Capsule } from "@react-three/drei";
 
 export default function CharacterController() {
+  const { WALK_SPEED, RUN_SPEED } = useControls("Character Control", {
+    WALK_SPEED: { value: 0.8, min: 0.1, max: 4, step: 0.1 },
+    RUN_SPEED: { value: 1.6, min: 0.2, max: 12, step: 0.1 },
+  });
+
+  const rb = useRef<Rapier.RigidBody | null>(null);
   const container = React.useRef<THREE.Group>(null);
   const cameraTarget = React.useRef<THREE.Group>(null);
   const cameraPosition = React.useRef<THREE.Group>(null);
@@ -16,7 +25,47 @@ export default function CharacterController() {
   const cameraLookAtWorldPosition = useRef(new THREE.Vector3());
   const cameraLookAt = useRef(new THREE.Vector3());
 
+  const [, get] = useKeyboardControls();
+
   useFrame(({ camera }) => {
+    /**
+     * Handle character movement
+     */
+    if (rb.current) {
+      const vel = rb.current.linvel();
+
+      const movement = {
+        x: 0,
+        z: 0,
+      };
+
+      if (get().forward) {
+        movement.z = 1;
+      }
+      if (get().backward) {
+        movement.z = -1;
+      }
+
+      const speed = get().run ? RUN_SPEED : WALK_SPEED;
+
+      if (get().left) {
+        movement.x = -1;
+      }
+      if (get().right) {
+        movement.x = 1;
+      }
+
+      // Apply the movement
+      if (movement.x || movement.z) {
+        vel.z = speed * movement.z;
+      }
+      rb.current.setLinvel(vel, true);
+    }
+
+    /**
+     * Handle camera movement
+     * The camera should follow the character but it should look at a point in front of the character
+     */
     if (
       !container.current ||
       !cameraTarget.current ||
@@ -27,16 +76,16 @@ export default function CharacterController() {
     cameraPosition.current.getWorldPosition(cameraWorldPosition.current);
     camera.position.lerp(cameraWorldPosition.current, 0.1);
 
-    if (cameraTarget.current) {
-      cameraTarget.current.getWorldPosition(cameraLookAtWorldPosition.current);
-      cameraLookAt.current.lerp(cameraLookAtWorldPosition.current, 0.1);
-      camera.lookAt(cameraLookAt.current);
-    }
+    // if (cameraTarget.current) {
+    cameraTarget.current.getWorldPosition(cameraLookAtWorldPosition.current);
+    cameraLookAt.current.lerp(cameraLookAtWorldPosition.current, 0.1);
+    camera.lookAt(cameraLookAt.current);
+    // }
   });
 
   return (
     <>
-      <RigidBody colliders={false}>
+      <RigidBody colliders={false} position-y={1} lockRotations ref={rb}>
         <group ref={container}>
           <group ref={cameraTarget} position-z={1.5} />
           <group ref={cameraPosition} position-y={2.5} position-z={-4} />
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { KeyboardControls } from "@react-three/drei";
 import "./App.css";
 import Experience from "./Experience";
+
+const keyboardMap = [
+  { name: "forward", keys: ["KeyW", "ArrowUp"] },
+  { name: "backward", keys: ["KeyS", "ArrowDown"] },
+  { name: "left", keys: ["KeyA", "ArrowLeft"] },
+  { name: "right", keys: ["KeyD", "ArrowRight"] },
+  { name: "run", keys: ["ShiftLeft", "ShiftRight"] },
+];
 
 function App() {
   return (
     <>
-      <Experience />
+      <KeyboardControls map={keyboardMap}>
+        <Experience />
+      </KeyboardControls>
     </>
   );
 }
